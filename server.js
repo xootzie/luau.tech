7e819a48dc81e85d8461d318c439f6cac4dd4a0c
xootzie <xootzie@gmail.com>
2024-12-11 21:26:33 -0500

    This is just a commit to initialize with Vercel

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ const start = async () => {
  
   const port = process.env.PORT || 3001;
   app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running http://localhost:${port}`);
   });
 };
 
